c61f6806792f213df41b48cce6c7fdc2140cf956
Kamalabot <kamaljp@gmail.com>
2022-10-19 11:03:23 +0000

    Starting the line chart

---
+++ src/lib/SvgChart.svelte
@@ -0,0 +1,8 @@
+<script>
+// @ts-nocheck
+
+    export let height;
+    export let width;
+</script>
+
+<svg width={width} height={height}></svg>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,9 @@
+<script>
+	// @ts-nocheck
+	import SvgChart from "$lib/SvgChart.svelte";
+</script>
+
+
 <svelte:head>
 	<title>Why Tracker</title>
 </svelte:head>
@@ -5,3 +11,5 @@
 <h1 class="dib tc bg-light-blue">About this site</h1>
 
 <p class="f4 lh-copy">This site is trial of the svelteKit to build a full fledged interactive dashboard. The data is taken from <a href=" https://api.covidtracking.com/v1/states/current.json">each states current data</a> and <a href="https://api.covidtracking.com/v1/us/current.json">Entire US data</a> </p>
+
+<SvgChart height={200} width={500}/>
